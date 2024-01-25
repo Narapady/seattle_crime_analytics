961cd357a6a24538cee6d805d328efa05dec66b4
Narapady <chhuoynarapady@yahoo.com>
2024-01-24 22:56:33 -0800

    fixed ingest schedule

--- console.py
+++ console.py
@@ -2,11 +2,8 @@ from src.client import Client
 from rich import print
 import pandas as pd
 
-# Convert to pandas DataFrame
 if __name__ == "__main__":
     client = Client()
-    df = client.make_request()
-    df.head()
-    # df.to_csv("./data/crime_raw.csv", index=False)
-
-    # print_json(data[0])
+    data = client.get_dataset()
+    # data.to_csv("./data/spd-crime-data-2023.csv", index=False)
+    print(data.head())
--- src/client.py
+++ src/client.py
@@ -3,6 +3,7 @@ import json
 from settings import APP_TOKEN, USER_NAME, PASSWORD
 import pandas as pd
 from sodapy import Socrata
+from datetime import datetime, timedelta
 
 
 # data from https://data.seattle.gov/Public-Safety/SPD-Crime-Data-2008-Present/tazs-3rd5/about_data
@@ -12,15 +13,22 @@ class Client:
     password = PASSWORD
     identitier = "tazs-3rd5"
 
-    def make_request(self) -> dict:
+    def get_dataset(self) -> pd.DataFrame:
+        # TODO: change date to ingest data daily
         client = Socrata(
             self.url,
             APP_TOKEN,
             username=self.username,
             password=self.password,
         )
-        client = Socrata(self.url, None)
-        results = client.get(self.identitier, limit=10000)
+        date = str(datetime.today().date())
+
+        # initial query="SELECT * WHERE date_extract_y(report_datetime) = 2023 LIMIT 100000"
+        # subsequence query is daily data
+        results = client.get(
+            self.identitier,
+            query=f"SELECT * WHERE date_trunc_ymd(report_datetime) = '{date}T00:00:00.000'",
+        )
         results_df = pd.DataFrame.from_records(results)
         return results_df
 
